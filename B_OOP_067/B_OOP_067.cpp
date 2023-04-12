e5e9ca0fc722acc5a28568d88971ca1e9fd0e5e8
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-04-12 10:34:31 +0700

    Add project files.

---
+++ B_OOP_067/B_OOP_067.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+private:
+	int nim;
+	string nama;
+	float nilai;
+public:
+	void input() {
+		cout << "Masukan NIM : ";
+		cin >> nim;
+		cout << "Masukan Nama : ";
+		cin >> nama;
+		cout << "Masukan Nilai : ";
+		cin >> nilai;
+	}
+	void printData();
+}
